f6c43d2e1517a53fd813c1380aab49419db9394a
Wojciech Spychalski <wspychalski00@gmail.com>
2022-07-27 21:11:47 +0200

    Implemented M16 - PrometheeGroupRanking

---
+++ ModularParts/PrometheeGroupRanking.py
@@ -0,0 +1,42 @@
+import numpy as np
+from core.aliases import NumericValue
+from typing import List, Tuple
+
+
+class PrometheeGroupRanking:
+    """
+    This class calculates aggregated flows which are weighted sum of flows for every alternative.
+    Allows many Decision Makers to get influence on final flows.
+    """
+
+    def __init__(self, alternatives: List[str], flows: List[NumericValue], weightsDMs: List[NumericValue]):
+        """
+        :param alternatives: List of alternatives names
+        :param flows: List of flows (one per alternative)
+        :param weightsDMs: List of weights of every Decision Maker. The greater the weight
+         the greater the influence of DM on the output flow.
+        """
+        self.alternatives = alternatives
+        self.flows = np.array(flows)
+        self.weightsDMs = np.array(weightsDMs)
+
+    def __calculate_weighted_flows(self) -> np.ndarray:
+        """
+        Calculates weighted flows by multiplying flows by each DM weight.
+        :return: ndarray(2 dim) of weighted flows
+        """
+        return np.array([weight * self.flows for weight in self.weightsDMs])
+        # return [[weight * flow_value for flow_value in flow] for weight, flow in zip(self.weightsDMs, self.flows)]
+
+    def calculate_group_ranking(self) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Calculates aggregated flows.
+        :return: Tuple of ndarray(2 dim) of aggregated flows and ndarray(2 dim) of weighted flows
+        """
+        weighted_flows = self.__calculate_weighted_flows()
+        aggregated_flows = np.sum(weighted_flows, axis=0)
+
+        return aggregated_flows, weighted_flows
+
+
+
