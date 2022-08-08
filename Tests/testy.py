dd650be676fd4e19749e7aeb8c84f408315cf713
Witold <wandraszyk73@gmail.com>
2022-08-08 21:18:21 +0200

    Profiles for M4 module and some doc updates

--- Tests/testy.py
+++ Tests/testy.py
@@ -42,25 +42,25 @@ print("Intervals:", intervals)
 print("Ranking M13_PrometheeIIIFlow:", pairs)
 
 # TEST M3 PROFILE
-# print("--------Test modułu M3 dla wariantów i profili----------")
-# #      G1   G2
-# ap = [[10, 12],  # a1
-#       [11, 13],  # a2
-#       [12, 14]]  # a3
-#
-# profile = ['b1', 'b2']
-# #      G1  G2
-# pp = [[11, 11],  # b1
-#       [14, 13]]  # b2
-#
-# aggregatedPI, partialPref = PrometheePreference(warianty, kryteria, ap, weights,
-#                                                 p_param, q_param, s_param, generalized_criteria,
-#                                                 directions, profile, pp).computePreferenceIndices()
-# print("partial pref", partialPref)
-# print("preferencje : ", aggregatedPI)
-#
-# positiveFlow, negativeFlow = PrometheeOutrankingFlows(warianty, aggregatedPI).calculate_flows(True)
-# print("positive flow: ", positiveFlow, "negative flow: ", negativeFlow)
-#
-# pairs = PrometheeIRanking(warianty, positiveFlow, negativeFlow).calculate_ranking()
-# print(pairs)
+print("--------Test modułu M3 dla wariantów i profili----------")
+#      G1   G2
+ap = [[10, 12],  # a1
+      [11, 13],  # a2
+      [12, 14]]  # a3
+
+profile = ['b1', 'b2']
+#      G1  G2
+pp = [[11, 11],  # b1
+      [14, 13]]  # b2
+
+aggregatedPI, partialPref = PrometheePreference(warianty, kryteria, ap, weights,
+                                                p_param, q_param, s_param, generalized_criteria,
+                                                directions, profile, pp).computePreferenceIndices()
+print("partial pref", partialPref)
+print("preferencje : ", aggregatedPI)
+
+positiveFlow, negativeFlow = PrometheeOutrankingFlows(warianty, aggregatedPI).calculate_flows(True)
+print("positive flow: ", positiveFlow, "negative flow: ", negativeFlow)
+
+pairs = PrometheeIRanking(warianty, positiveFlow, negativeFlow).calculate_ranking()
+print(pairs)
