57c06caa465203cd25de5971e928a8d4913f108a
Wojciech Spychalski <wspychalski00@gmail.com>
2022-10-29 15:56:23 +0200

    Add test to M16 and M10; small bug fixes

--- Tests/test_importa.py
+++
@@ -1,3 +0,0 @@
-from modular_parts.weights import *
-
-calculate_srf_weights(criteria_ranks, criteria_weight_ratio, decimal_place)
--- modular_parts/flows/M16_PrometheeGroupRanking.py
+++ modular_parts/flows/M16_PrometheeGroupRanking.py
@@ -9,26 +9,17 @@ from core.aliases import DMsTable
 __all__ = ['calculate_promethee_group_ranking']
 
 
-def _calculate_weighted_flows(dms_data: DMsTable) -> np.ndarray:
-    """
-    Calculates weighted flows by multiplying flows by each DM weight.
-    :return: ndarray(2 dim) of weighted flows
-    """
-    return np.multiply(dms_data['weights'], dms_data[dms_data.columns.difference(['weights'])].T).T
-
-
-def calculate_promethee_group_ranking(dms_data: DMsTable) -> pd.DataFrame:
+def calculate_promethee_group_ranking(dms_flows: pd.DataFrame, dms_weights: pd.Series) -> pd.Series:
     """
     Calculates aggregated flows.
 
-    :param dms_data: DMsTable of DMs flows (positive or negative) and weights values.
+    :param dms_flows: DataFrame with DMs as columns and alternatives as rows
+    :param dms_weights: Series with DMs as index and weights as values
 
     :return: DataFrame with aggregated flows(column 'aggregated') and  weighted flows(column 'weighted')
     """
-    dms_data = dms_data.T
+    weighted_flows = dms_flows.mul(dms_weights, axis=1)
+    aggregated_flows = weighted_flows.sum(axis=1)
+    aggregated_flows.name = 'aggregated_flows'
 
-    alternatives = dms_data.index
-    weighted_flows = _calculate_weighted_flows(dms_data)
-    aggregated_flows = np.sum(weighted_flows, axis=0)
-
-    return pd.DataFrame([weighted_flows, aggregated_flows], columns=['weighted', 'aggregated'], index=alternatives)
+    return aggregated_flows
