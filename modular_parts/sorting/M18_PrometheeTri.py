0892ba6aade94f2dc0f8820a5acf1791f14d5b14
Wojciech Spychalski <wspychalski00@gmail.com>
2022-11-11 23:57:18 +0100

    Add M18 test

--- modular_parts/sorting/M18_PrometheeTri.py
+++ modular_parts/sorting/M18_PrometheeTri.py
@@ -27,7 +27,7 @@ def _calculate_criteria_net_flows(alternatives_partial_preferences: Tuple[pd.Dat
     return profiles_criteria_net_flows, alternatives_criteria_net_flows
 
 
-def _calculate_deviations(alternatives: List[str], profiles_partial_preferences: pd.DataFrame,
+def _calculate_deviations(alternatives: List[str],
                           criteria_weights: pd.Series, profiles_criteria_net_flows: pd.DataFrame,
                           alternatives_criteria_net_flows: pd.DataFrame,
                           use_marginal_value: bool = True) -> pd.DataFrame:
@@ -35,7 +35,6 @@ def _calculate_deviations(alternatives: List[str], profiles_partial_preferences:
     Calculate deviation for each alternative and each profile.
 
     :param alternatives: List of alternatives (strings only)
-    :param profiles_partial_preferences: DataFrame with partial preferences (profiles vs profiles)
     :param criteria_weights: Series with weights of each criterion
     :param profiles_criteria_net_flows: DataFrame with criteria net flows for profiles
     :param alternatives_criteria_net_flows: DataFrame with criteria net flows for alternatives
@@ -44,23 +43,25 @@ def _calculate_deviations(alternatives: List[str], profiles_partial_preferences:
     :return: DataFrame with deviations
     """
 
-    deviations = pd.DataFrame(columns=profiles_partial_preferences.index, index=alternatives)
+    deviations = pd.DataFrame(columns=profiles_criteria_net_flows.index, index=alternatives, dtype=float)
 
     for alternative, alternative_row in alternatives_criteria_net_flows.iterrows():
         for profile, profile_row in profiles_criteria_net_flows.iterrows():
             if use_marginal_value:
-                deviations[profile][alternative] = abs(alternative_row - profile_row).sum()
+                deviations.loc[alternative, profile] = (criteria_weights * abs(alternative_row - profile_row)).sum()
             else:
-                deviations[profile][alternative] = (criteria_weights * (alternative_row - profile_row)).sum()
+                deviations.loc[alternative, profile] = (criteria_weights * (alternative_row - profile_row)).sum()
 
     return deviations
 
 
-def _assign_alternatives_to_classes_with_minimal_deviation(alternatives: List[str], deviations: pd.DataFrame,
+def _assign_alternatives_to_classes_with_minimal_deviation(categories: List[str],
+                                                           alternatives: List[str], deviations: pd.DataFrame,
                                                            assign_to_better_class: bool = True) -> pd.Series:
     """
     Assign every alternative to class with minimal deviation for pair alternative, class.
 
+    :param categories: List of categories (strings only)
     :param alternatives: List of alternatives (strings only)
     :param deviations: DataFrame with deviations
     :param assign_to_better_class: Boolean which describe preference of the DM in final alternative assignment when
@@ -68,17 +69,17 @@ def _assign_alternatives_to_classes_with_minimal_deviation(alternatives: List[st
 
     :return: Series with precise assignments of alternatives to categories
     """
-    classification = pd.Series(index=alternatives)
+    classification = pd.Series(index=alternatives, dtype=str)
     for alternative, alternative_row in deviations.iterrows():
         if assign_to_better_class:
-            classification[alternative] = alternative_row.contains(alternative_row.min()).idxmax()
+            classification[alternative] = categories[alternative_row.argmin()]
         else:
-            classification[alternative] = alternative_row.contains(alternative_row.min()).idxmin()
+            classification[alternative] = categories[alternative_row[::-1].argmin()]
 
     return classification
 
 
-def calculate_prometheetri_sorted_alternatives(criteria_weights: pd.Series,
+def calculate_prometheetri_sorted_alternatives(categories: List[str], criteria_weights: pd.Series,
                                                alternatives_partial_preferences: Tuple[pd.DataFrame, pd.DataFrame],
                                                profiles_partial_preferences: pd.DataFrame,
                                                assign_to_better_class: bool = True,
@@ -86,6 +87,7 @@ def calculate_prometheetri_sorted_alternatives(criteria_weights: pd.Series,
     """
     Sort alternatives to proper categories.
 
+    :param categories: List of categories (strings only)
     :param criteria_weights: Series with weights of each criterion
     :param alternatives_partial_preferences: Tuple with 2 DataFrames with partial preferences (alternatives vs profiles
     and profiles vs alternatives)
@@ -101,6 +103,9 @@ def calculate_prometheetri_sorted_alternatives(criteria_weights: pd.Series,
 
     profiles_criteria_net_flows, alternatives_criteria_net_flows = \
         _calculate_criteria_net_flows(alternatives_partial_preferences, profiles_partial_preferences)
-    deviations = _calculate_deviations(alternatives, profiles_partial_preferences, criteria_weights,
+
+    deviations = _calculate_deviations(alternatives, criteria_weights,
                                        profiles_criteria_net_flows, alternatives_criteria_net_flows, use_marginal_value)
-    return _assign_alternatives_to_classes_with_minimal_deviation(alternatives, deviations, assign_to_better_class)
+
+    return _assign_alternatives_to_classes_with_minimal_deviation(categories, alternatives, deviations,
+                                                                  assign_to_better_class)
