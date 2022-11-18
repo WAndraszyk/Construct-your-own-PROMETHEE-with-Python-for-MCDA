8ff3f733ac73d7fb0b9ed6918577a5b11b3d5886
Tobias2705 <t_gruszczynski@op.pl>
2022-11-18 20:36:44 +0100

    M10 input validation

---
+++ core/input_validation/__init__.py
@@ -0,0 +1,7 @@
+"""
+    This subpackage contains input validation for all structures.
+"""
+
+from .alternatives_profiles_input_validation import *
+
+__all__ = alternatives_profiles_input_validation.__all__
---
+++ core/input_validation/alternatives_profiles_input_validation.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+__all__ = ["alternatives_profiles_validation"]
+
+
+def _check_weights(weights: pd.Series, criteria_num: int):
+    if not isinstance(weights, pd.Series):
+        raise ValueError("Criteria weights should be passed as a Series object")
+
+    if len(weights) != criteria_num:
+        raise ValueError("Number of weights should be equals to number of criteria")
+
+    for weight in weights:
+        if not isinstance(weight, (int, float)):
+            raise ValueError("Weights should be a numeric values")
+
+
+def _check_partial_preferences(partial_preferences: pd.DataFrame):
+    if not isinstance(partial_preferences, pd.DataFrame):
+        raise ValueError("Partial preferences should be passed as a DataFrame object")
+
+
+def _check_criteria(criteria_from_series: pd.Index, criteria_from_df: pd.Index):
+    if not criteria_from_series.equals(criteria_from_df):
+        raise ValueError("Criteria defined in criteria weights should be the same as in partial preferences")
+
+
+def alternatives_profiles_validation(criteria_weights: pd.Series, partial_preferences: pd.DataFrame):
+    criteria_from_df = partial_preferences.index.get_level_values(0).unique()
+    criteria_from_series = criteria_weights.index
+
+    _check_weights(criteria_weights, len(criteria_from_df))
+    _check_partial_preferences(partial_preferences)
+    _check_criteria(criteria_from_series, criteria_from_df)
