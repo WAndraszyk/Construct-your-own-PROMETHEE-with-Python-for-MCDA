11cc4039ff71ece93c35550234488d16006935a0
Tobias2705 <t_gruszczynski@op.pl>
2022-11-18 21:22:09 +0100

    M11 input validation

--- core/input_validation/__init__.py
+++ core/input_validation/__init__.py
@@ -3,5 +3,6 @@
 """
 
 from .alternatives_profiles_input_validation import *
+from .ranking_input_validation import *
 
-__all__ = alternatives_profiles_input_validation.__all__
+__all__ = alternatives_profiles_input_validation.__all__ + ranking_input_validation.__all__
