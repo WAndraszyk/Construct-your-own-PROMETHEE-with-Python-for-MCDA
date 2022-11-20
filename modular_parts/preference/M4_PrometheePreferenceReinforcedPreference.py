0773fa251ecdc5b0d026c2f22e4ad01d53e659fa
Witold Andraszyk <wandraszyk73@gmail.com>
2022-11-20 16:19:47 +0100

    Preference direction enum

--- modular_parts/preference/M4_PrometheePreferenceReinforcedPreference.py
+++ modular_parts/preference/M4_PrometheePreferenceReinforcedPreference.py
@@ -41,7 +41,7 @@ def compute_reinforced_preference(alternatives_performances: PerformanceTable,
     :return: preferences
     :return: partial preferences
     """
-    criteria = weights.keys()
+    criteria = weights.index
     alternatives_performances = pc.directed_alternatives_performances(alternatives_performances, directions)
     weights = weights
     generalized_criteria = generalized_criteria
