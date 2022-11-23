a81901bf54760440ecc5e69168a66dd879cd357a
Witold Andraszyk <wandraszyk73@gmail.com>
2022-11-23 22:16:53 +0100

    File name change

--- core/input_validation/__init__.py
+++ core/input_validation/__init__.py
@@ -7,8 +7,8 @@ from .ranking_input_validation import *
 from .flow_input_validation import *
 from .sorting_input_validation import *
 from .weights_input_validation import *
-from .preference_validation import *
+from .preference_input_validation import *
 
 __all__ = alternatives_profiles_input_validation.__all__ + ranking_input_validation.__all__ + \
           flow_input_validation.__all__ + sorting_input_validation.__all__ + weights_input_validation.__all__ + \
-          preference_validation.__all__
+          preference_input_validation.__all__
