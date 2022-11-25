891de92d49615423ee9bc2a9c90cc833afab61a4
Witold Andraszyk <wandraszyk73@gmail.com>
2022-11-26 00:51:29 +0100

    M4 name changed

--- modular_parts/preference/__init__.py
+++ modular_parts/preference/__init__.py
@@ -1,8 +1,8 @@
 from .M3_PrometheePreference import *
-from .M4_PrometheePreferenceReinforcedPreference import *
+from .M4_PrometheeReinforcedPreference import *
 from .M5_PrometheePreferenceWithInteractions import *
 from .M6_PrometheeDiscordance import *
 from .M7_PrometheeVeto import *
 
-__all__ = M3_PrometheePreference.__all__ + M4_PrometheePreferenceReinforcedPreference.__all__ + \
+__all__ = M3_PrometheePreference.__all__ + M4_PrometheeReinforcedPreference.__all__ + \
           M5_PrometheePreferenceWithInteractions.__all__ + M6_PrometheeDiscordance.__all__ + M7_PrometheeVeto.__all__
