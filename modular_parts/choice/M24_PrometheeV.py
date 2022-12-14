effb76c69a8be65ff56084643f1c390c6df823cb
Witold Andraszyk <wandraszyk73@gmail.com>
2022-12-14 18:39:51 +0100

    M24 doc patch

--- modular_parts/choice/M24_PrometheeV.py
+++ modular_parts/choice/M24_PrometheeV.py
@@ -17,18 +17,22 @@ def compute_decision(flows: pd.Series, constraints: List[Constraint]
     """
     Computes decision by solving a linear problem.
 
-    :param flows: net flows
+    :param flows: Series of net flows with alternatives as index
     :param constraints: list of problem constraints
 
-    :returns: alternatives which are part of the decision
+    :returns: Series of alternatives which are part of the decision
     """
+    # input data validation
     decision_validation(flows, constraints)
 
     alternatives = flows.index
     flows = flows.values
     constraints = constraints
 
+    # solve linear problem
     decision_tuple = solve_linear_problem(constraints, flows, len(flows))
+
+    # find alternatives that create the solution
     chosen_alternatives = []
     for i in range(len(decision_tuple)):
         if decision_tuple[i] == 1:
